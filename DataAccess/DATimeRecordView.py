75e5b4db26dbcc5096db717d633b9ab4e28cc7f1
willemserruys <willem.serruys@vinci-energies.net>
2019-01-09 00:10:11 +0100

    View graphs of time records - trial

--- DataAccess/DATimeRecordView.py
+++ DataAccess/DATimeRecordView.py
@@ -1,16 +1,19 @@
 import sqlite3
 from BusinessEntities import TimeRecordView
 
+
 class DATimeRecordView:
-    def __init__(self,conn):
+    def __init__(self, conn):
         self.Cursor = conn.Connection.cursor()
-    
-    def GetByID(self,Id):
-        self.Cursor.execute("SELECT * FROM vwTimeRecord where TRE_ID = ?",(str(Id),))
+
+    def GetByID(self, Id):
+        self.Cursor.execute(
+            "SELECT * FROM vwTimeRecord where TRE_ID = ?", (str(Id),))
         rows = self.Cursor.fetchall()
         TimeRecords = []
         for row in rows:
-            timeRecord = TimeRecordView.TimeRecordView(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],str(row[8]),row[9])
+            timeRecord = TimeRecordView.TimeRecordView(
+                row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], str(row[8]), row[9], row[10])
             TimeRecords.append(timeRecord)
         return TimeRecords[0]
 
@@ -18,22 +21,26 @@ class DATimeRecordView:
         idList = []
         for Id in Ids:
             idList.append(self.GetByID(Id))
-        return idList        
+        return idList
 
     def GetAll(self):
-        self.Cursor.execute("SELECT * FROM vwTimeRecord order by StartTime DESC")
+        self.Cursor.execute(
+            "SELECT * FROM vwTimeRecord order by StartTime DESC")
         rows = self.Cursor.fetchall()
         TimeRecordViews = []
         for row in rows:
-            timeRecordview = TimeRecordView.TimeRecordView(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],str(row[8]),row[9])
+            timeRecordview = TimeRecordView.TimeRecordView(
+                row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], str(row[8]), row[9], row[10])
             TimeRecordViews.append(timeRecordview)
         return TimeRecordViews
 
-    def GetAllBetweenDates(self,fromDate,toDate):
-        self.Cursor.execute("SELECT * from vwTimeRecord where TRE_ID in (SELECT TRE_ID from tblTimeRecord where TRE_StartHour between ? and ?)",(fromDate,toDate))
+    def GetAllBetweenDates(self, fromDate, toDate):
+        self.Cursor.execute(
+            "SELECT * from vwTimeRecord where TRE_ID in (SELECT TRE_ID from tblTimeRecord where TRE_StartHour between ? and ?)", (fromDate, toDate))
         rows = self.Cursor.fetchall()
         TimeRecordViews = []
         for row in rows:
-            timeRecordview = TimeRecordView.TimeRecordView(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],str(row[8]),row[9])
+            timeRecordview = TimeRecordView.TimeRecordView(
+                row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], str(row[8]), row[9], row[10])
             TimeRecordViews.append(timeRecordview)
         return TimeRecordViews
--- GUI/ExportToExcelForm.py
+++ GUI/ExportToExcelForm.py
@@ -1,11 +1,15 @@
 from tkinter import *
-from tkinter import ttk,messagebox,filedialog
-from BusinessLogic import BLExcel,BLTimeRecordView,Globals
+from tkinter import ttk, messagebox, filedialog
+from BusinessLogic import BLExcel, BLTimeRecordView, Globals
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+from python_linq import From
 
 
 class ExportToExcelForm:
-    def __init__(self,conn):
+    def __init__(self, conn):
         master = Toplevel()
         self.Master = master
         self.Connection = conn
@@ -14,37 +18,65 @@ class ExportToExcelForm:
 
         self.Master.title("Export To Excel")
 
-        self.ExplanationLabel = Label(master,text='Export to excel between dates')
-        self.ExplanationLabel.grid(row = 0,column = 0, columnspan = 2)
+        self.ExplanationLabel = Label(
+            master, text='Export to excel between dates')
+        self.ExplanationLabel.grid(row=0, column=0, columnspan=2)
 
-        self.DateFromLabel = Label(master,text = 'Select FROM date (yyyy-mm-dd):')
-        self.DateFromLabel.grid(row=1,column=0)
+        self.DateFromLabel = Label(
+            master, text='Select FROM date (yyyy-mm-dd):')
+        self.DateFromLabel.grid(row=1, column=0)
 
-        self.DateFromTextBox = Entry(master,textvariable = self.DateFrom)
-        self.DateFromTextBox.grid(row=1,column = 1,sticky='NSEW')
+        self.DateFromTextBox = Entry(master, textvariable=self.DateFrom)
+        self.DateFromTextBox.grid(row=1, column=1, sticky='NSEW')
 
-        self.DateFromLabel = Label(master,text = 'Select TO date (yyyy-mm-dd):')
-        self.DateFromLabel.grid(row=2,column=0)
+        self.DateFromLabel = Label(master, text='Select TO date (yyyy-mm-dd):')
+        self.DateFromLabel.grid(row=2, column=0)
 
-        self.DateToTextBox = Entry(master,textvariable = self.DateTo)
-        self.DateToTextBox.grid(row=2,column = 1,sticky='NSEW')
+        self.DateToTextBox = Entry(master, textvariable=self.DateTo)
+        self.DateToTextBox.grid(row=2, column=1, sticky='NSEW')
 
-        self.OKButton = Button(master,text="OK",command = self.Confirm)
-        self.OKButton.grid(row = 3,column = 0,sticky='NSEW')
+        self.OKButton = Button(master, text="OK", command=self.Confirm)
+        self.OKButton.grid(row=3, column=0, sticky='NSEW')
 
-    
     def Confirm(self):
         blE = BLExcel.BLExcel(self.Connection)
         fromDate = self.DateFrom.get()
         toDate = self.DateTo.get()
         blTrV = BLTimeRecordView.BLTimeRecordView(self.Connection)
-        timeRecords = blTrV.GetAllBetweenDates(fromDate,toDate)
-        result = messagebox.askquestion("Validatie", "{0} records geselecteerd. Doorgaan?".format(len(timeRecords)), icon='warning')
+        timeRecords = blTrV.GetAllBetweenDates(fromDate, toDate)
+        self.CreateGraph(timeRecords)
+        result = messagebox.askquestion(
+            "Validatie", "{0} records geselecteerd. Doorgaan?".format(len(timeRecords)), icon='warning')
         if result == 'yes':
             file_path = filedialog.askdirectory()
             file_path = file_path + "/" + Globals.GetCurrentDay()
-            blE.ExportToExcel(timeRecords,file_path)
+            blE.ExportToExcel(timeRecords, file_path)
             self.Master.quit()
 
     def Show(self):
         self.Master.mainloop()
+
+    def CreateGraph(self, timeRecords):
+        # Make a fake dataset:
+        records = (From(timeRecords).groupBy(
+            lambda x: x.Project,
+            transform=lambda x: x.Minutes
+        ))
+
+        bars = []
+        height = []
+        for record in records:
+            bars.append(record.key)
+            height.append(From(record).sum(lambda x: x))
+        print(bars)
+        print(height)
+        y_pos = np.arange(len(bars))
+
+        # Create bars
+        plt.bar(y_pos, height)
+
+        # Create names on the x-axis
+        plt.xticks(y_pos, bars)
+
+        # Show graphic
+        plt.show()
